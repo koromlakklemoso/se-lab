895b15bf08580a627d2c0a1201c1208f3e4fc3bd
unknown <176032@BME.HU>
2024-10-09 09:36:55 +0200

    I wrote a comment in English

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,7 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-  //kivettem a függvényből
+  //I took it out from the function
   Random generator = new Random();
   private int torpedoCount = 0;
 
